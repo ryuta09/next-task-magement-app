5d08b04d684110eaa96c37e73f94eff0c954cd7f
ryuta09 <gako0918@gmail.com>
2025-11-30 21:40:16 +0900

    taskIdがなかった時とtaskがない時はnullではなくnotFoundを返すように変更

--- app/(main)/edit/[id]/page.tsx
+++ app/(main)/edit/[id]/page.tsx
@@ -1,14 +1,23 @@
 import EditTaskForm from '@/components/EditTaskForm/EditTaskForm'
 import { getTaskById } from '@/lib/api'
+import { notFound } from 'next/navigation'
 
 interface Props {
-	params: Promise<{ id: number }>
+	params: Promise<{ id: string }>
 }
 export default async function TaskEditPage({ params }: Props) {
 	const { id } = await params
 	const taskId = Number(id)
+	
+	if (isNaN(taskId)) {
+		notFound()
+	}
+	
 	const task = await getTaskById(taskId)
-	if (!task) return null
+	if (!task) {
+		notFound()
+	}
+	
 	return (
 		<div className='flex flex-col justify-center py-20'>
 			<h2 className='text-center font-bold text-2xl'>編集</h2>
